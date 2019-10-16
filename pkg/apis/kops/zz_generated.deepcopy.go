9baf43f93e3363d7ccea63398d6e0414677254c6
Ripta Pasay <ripta@users.noreply.github.com>
2019-10-15 23:27:28 -0700

    Update apimachinery

--- pkg/apis/kops/zz_generated.deepcopy.go
+++ pkg/apis/kops/zz_generated.deepcopy.go
@@ -861,6 +861,11 @@ func (in *ClusterSpec) DeepCopyInto(out *ClusterSpec) {
 		*out = new(TargetSpec)
 		(*in).DeepCopyInto(*out)
 	}
+	if in.SysctlParameters != nil {
+		in, out := &in.SysctlParameters, &out.SysctlParameters
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
 	return
 }
 
@@ -1702,6 +1707,11 @@ func (in *InstanceGroupSpec) DeepCopyInto(out *InstanceGroupSpec) {
 		in, out := &in.InstanceProtection, &out.InstanceProtection
 		*out = new(bool)
 		**out = **in
+	}
+	if in.SysctlParameters != nil {
+		in, out := &in.SysctlParameters, &out.SysctlParameters
+		*out = make([]string, len(*in))
+		copy(*out, *in)
 	}
 	return
 }
